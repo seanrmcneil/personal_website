fe0b8abe3e4ba599012bc9d199139a0bb56bd206
Sean McNeil <seanmcneil@Seans-MacBook-Pro.local>
2018-10-21 11:57:57 -0400

    Banner photo and intro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import image from './images/A04I2281.jpg';
+
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <AppBar position="static" color="default" position="fixed">
+                <Toolbar>
+                  <Typography variant="h6" color="inherit">
+                    Sean McNeil
+                  </Typography>
+                </Toolbar>
+              </AppBar>
+         <div>
+            <img className="Banner-image" src={image} />
+          </div>
+          <div className = "Introduction-section">
+          Hello! I am a Full Stack Engineer. <br />
+          </div>
       </div>
     );
   }
